625785a6b60716596b9c9d7bf7814dc56165c542
PrasathGowtham <108874931+PrasathGowtham@users.noreply.github.com>
2022-12-15 16:14:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Login from "./Modules/login";
+import "./app.css"
+import Signup from "./Modules/singnup";
+import Homepage, { Token} from "./Modules/Generator/homepage";
+import Item from "./context/context";
+import Routerpath from "./Routes/route";
+import Food from "./Modules/Generator/token";
+import Cart from "./Modules/Generator/cart";
+function App() {
+  return (
+    <div className="App">
+      
+<Routerpath/>
+   
+    </div>
+  );
+}
+
+
+export default App;
